4fde466c9e50647ceb9fad53177af931bcbcb5da
trinuro <66290935+trinuro@users.noreply.github.com>
2024-08-01 19:20:23 +0800

    Fix navbar button not working bug

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,11 +17,11 @@ const colourOfDots = ['black', 'red', 'yellow']
 
 const Hero = () => {
     return(
-        <section id='Home' className='flex-nowrap bg-cover bg-no-repeat bg-dun-and-bcs bg-center'>
+        <section id='Home' className='relative flex-nowrap bg-cover bg-no-repeat bg-dun-and-bcs bg-center -z-20'>
             <div className=' max-container padding-container pt-5 lg:pt-20 bg-white/50'>
                 <div className='text-center lg:bold-64 flex flex-col whitespace-nowrap gap-0'>
                     <p className='font-leagueSpartan text-[30px] font-extrabold leading-3 lg:text-[80px]'>DISKUSI BELANJAWAN</p>
-                    <p className='font-horta text-[100px] p-0 m-0 leading-tight text-yellow-dbsYellow lg:text-[300px] drop-shadow-xl [text-shadow:_#ff3131_2px_5px_3px] lg:[text-shadow:_#ff3131_4px_10px_3px]'>SARAWAK</p>
+                    <p id='SARAWAK_TEXT' className='font-horta text-[100px] p-0 m-0 leading-tight text-yellow-dbsYellow lg:text-[300px] drop-shadow-xl [text-shadow:_#ff3131_2px_5px_3px] lg:[text-shadow:_#ff3131_4px_10px_3px]'>SARAWAK</p>
                 
                     
                 </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,26 +6,28 @@ import {headers} from '@/Constants';
 
 const Navbar = () =>{
     return (
-        <nav className='bg-dayak-motif-light relative -z-10'>
-            <div className='absolute w-full h-full bg-white/95 -z-10'></div>
-            <div className='px-8 lg:pt-3 py-5 w-max lg:w-[80%]  align-middle max-container padding-container flex-col '>
-                <div className='sm:flex justify-between items-center'>
-                    <div className='flex flex-column items-center'>
-                        <Image id='logo'  src='/DUN2.png' alt='DBS Logo' width={80} height={25} className='lg:w-28 h-auto lg:flex-1  cursor-pointer active:scale-75 transition' ></Image>
-                        <p className='hidden lg:inline lg:regular-16 lg: font-hightide'>Diskusi Belanjawan Sarawak</p>
+        <>
+            <nav className='bg-dayak-motif-light relative opacity-90'>
+                
+                <div className='px-8 lg:pt-3 py-5 w-max lg:w-[80%]  align-middle max-container padding-container flex-col z-30'>
+                    <div className='sm:flex justify-between items-center'>
+                        <div className='flex flex-column items-center'>
+                            <Image id='logo'  src='/DUN2.png' alt='DBS Logo' width={80} height={25} className='lg:w-28 h-auto lg:flex-1  cursor-pointer active:scale-75 transition' ></Image>
+                            <p className='hidden lg:inline lg:regular-16 lg: font-hightide'>Diskusi Belanjawan Sarawak</p>
+                        </div>
+                        <ul className='sm:flex gap-8 hidden px-10'>
+                            {headers.map((heading, index)=>(
+                                <li key={heading+'_'+index}>
+                                    <Link href={'#'+heading} className='text-gray-500 font-bold lg:bold-18 hover:text-black-sarawak transition'>
+                                        {heading}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <ul className='sm:flex gap-8 hidden px-10'>
-                        {headers.map((heading, index)=>(
-                            <li key={heading+'_'+index}>
-                                <Link href={'#'+heading} className='text-gray-500 font-bold lg:bold-18 hover:text-black-sarawak transition'>
-                                    {heading}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
                 </div>
-
-                <ul id='navbar_expanded' className='hidden gap-6 lg:gap-16 px-10 left-0 flex-col text-center m-auto bg-white/80 w-full h-full z-10 absolute'>
+                <div className='bg-white/90 h-full w-full -z-10 absolute top-0'></div>
+                <ul id='navbar_expanded' className='hidden z-100 gap-6 lg:gap-16 px-10 left-0 flex-col text-center m-auto bg-white/80 w-screen h-screen absolute'>
                     {headers.map((heading, index)=>(
                         <li key={heading+'_toggle_'+index} className='pt-5'>
                             <Link href={'#'+heading} className='items-in-navbar text-gray-800 bold-32 lg:bold-52 hover:text-black-sarawak transition'>
@@ -34,33 +36,34 @@ const Navbar = () =>{
                         </li>
                     ))}
                 </ul>
-
-                <Script id="show_navbar" strategy='lazyOnload'>
-                    {
-                        `const navbar = document.getElementById('navbar_expanded');
-                        const logo =  document.getElementById('logo');
-                        const body = document.querySelector('body');
-                        const navbarButtons = [...document.getElementsByClassName('items-in-navbar')];
-                        isNavbarShowing = false;
-                        const hideNavbar = () =>{
-                            isNavbarShowing = !isNavbarShowing;
-                            if(isNavbarShowing){
-                                navbar.style.display = "flex";
-                                body.style.overflow='hidden';
-
-                            }else{
-                                navbar.style.display = "none";
-                                body.style.overflow='visible';
-                            }
-                        };
-                        logo.addEventListener('click', hideNavbar);
-                        navbarButtons.map((button)=>{
-                        button.addEventListener('click', hideNavbar);
-                        })`
-                    }
-                </Script>
-            </div>
-        </nav>
+            </nav>
+            <Script id="show_navbar" strategy='lazyOnload'>
+                {
+                    `const navbar = document.getElementById('navbar_expanded');
+                    const logo =  document.getElementById('logo');
+                    const body = document.querySelector('body');
+                    const navbarButtons = [...document.getElementsByClassName('items-in-navbar')];
+                    const sarawakText = document.getElementById('SARAWAK_TEXT');
+                    isNavbarShowing = false;
+                    const hideNavbar = () =>{
+                        isNavbarShowing = !isNavbarShowing;
+                        console.log(isNavbarShowing);
+                        if(isNavbarShowing){
+                            navbar.style.display = "flex";
+                            body.style.overflow='hidden';
+                        }else{
+                            navbar.style.display = "none";
+                            body.style.overflow='visible';
+                        }
+                    };
+                    logo.addEventListener('click', hideNavbar);
+                    navbarButtons.map((button)=>{
+                    button.addEventListener('click', hideNavbar);
+                    })`
+                }
+            </Script>
+            
+        </>
     )
 }
 
